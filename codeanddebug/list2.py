c098a5c56b2bcfc1e33cdea7773bbe1f9d48e59d
vinayaksingh262 <vs2622003@gmail.com>
2025-02-09 16:41:23 +0530

    new2

--- codeanddebug/list2.py
+++ codeanddebug/list2.py
@@ -2,5 +2,5 @@ x = [55, 45, "VSR", 55.8, -98]
 for i in range(0, 5):
     print(x[i], end=" ")
 for i in range(0, len(x)):
-    print(x[i])
+    print(x[i], end=", ")
 # iterate by position
--- codeanddebug/oops/class2.py
+++ codeanddebug/oops/class2.py
@@ -8,6 +8,11 @@ class Bank:
         self.phone = int(input("Enter your phone number:"))
         self.balance = 0
 
+    def show_info(self):
+        print(f"account no : {self.account}")
+        print(f"name  : {self.name}")
+        print(f"balance  : {self.balance}\n")
+
     def show_balance(self):
         print(f"current balance ={self.balance}")
 
@@ -31,10 +36,55 @@ class Bank:
 # b1.show_balance()
 # for multiple objects we have to create a lsit
 banks = []
-x = Bank()
-banks.append(x)
-y = Bank()
-banks.append(y)
-banks[0].show_balance()
-banks[1].deposite()
-banks[1].show_balance()
+# x = Bank()
+# banks.append(x)
+# y = Bank()
+# banks.append(y)
+# banks[0].show_balance()
+# banks[1].deposite()
+# banks[1].show_balance()
+# banks[1].deposite()
+# banks[1].show_balance()
+while True:
+    print("1.Create account")
+    print("2.show all accounts detail")
+    print("3.Deposite ammount ")
+    print("4.withdraw ammount ")
+    print("5.transfer ammount ")
+    print("6.Exit")
+    choice = int(input("Enter a choice ="))
+    if choice == 1:
+        obj = Bank()
+        banks.append(obj)
+    elif choice == 2:
+        if len(banks) == 0:
+            print("not account have been created yet...")
+        else:
+            for account in banks:
+                account.show_info()
+    elif choice == 3:
+        if len(banks) == 0:
+            print("not account have been created yet...")
+        else:
+            acc_no = int(input("enter account number to deposite:"))
+            for obj in banks:
+                if obj.account == acc_no:
+                    obj.deposite()
+                    break
+    elif choice == 4:
+        if len(banks) == 0:
+            print("no accounts have been created yet")
+        else:
+            acc_no = int(input("Enter account to withdraw money:"))
+            for obj in banks:
+                if obj.account == acc_no:
+                    obj.withdraw()
+                    break
+
+    elif choice == 5:
+        pass
+
+    elif choice == 6:
+        break
+    else:
+        print("invalid choice...")
