0fc8f2a9caac4e7d68c80ed9e7f2f6b20b8d3b6f
vinayaksingh262 <vs2622003@gmail.com>
2025-03-07 21:06:27 +0530

    tail

--- codeanddebug/oops/recursion.py
+++ codeanddebug/oops/recursion.py
@@ -7,3 +7,14 @@ def factorial(n):
 
 
 print(factorial(5))
+
+
+# tail
+def tail_factorial(n, result=1):
+    if n == 1:
+        return result
+    else:
+        return tail_factorial(n - 1, result * n)
+
+
+print(tail_factorial(5))
