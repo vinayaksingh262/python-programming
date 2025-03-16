904786e439b201c052e465423ce0a5fde9455c9c
Vinayak Singh <vs2622003@gmail.com>
2025-03-16 23:19:16 +0530

    nothing

--- codeanddebug/Q3.py
+++ codeanddebug/Q3.py
@@ -1,12 +1,12 @@
 # given two strings A and B,removes all characters in a that are presents in b and print the resulting string c
 
-# a = input().strip()
-# b = input().strip()
-# result = ""
-# for char in a:
-#     if char not in b:
-#         result += char
-# print(result)
+a = input().strip()
+b = input().strip()
+result = ""
+for char in a:
+    if char not in b:
+        result += char
+print(result)
 
 a = input().strip()
 b = input().strip()
