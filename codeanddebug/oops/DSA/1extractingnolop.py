bb870a9027320347c41f99ac277d0710a2173e26
Vinayak Singh <vs2622003@gmail.com>
2025-03-18 23:09:32 +0530

    changes in code

--- codeanddebug/oops/DSA/1extractingnolop.py
+++ codeanddebug/oops/DSA/1extractingnolop.py
@@ -1,6 +1,10 @@
-n = 5873
-num = n
-while num > 0:
-    last_digit = num % 10
-    print(last_digit)
-    num = num // 10
+def extractDigit(num: int) -> None:
+
+    n = num
+    while n > 0:
+        last_digit = n % 10
+        print(last_digit, end="")
+        n = n // 10
+
+
+extractDigit(5473)
