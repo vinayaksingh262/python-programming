81a4338b9c69813f160a3dead1267621827c2c4e
Vinayak Singh <vs2622003@gmail.com>
2025-03-22 01:11:08 +0530

    freqmap2

--- codeanddebug/oops/DSA/practice.py
+++ codeanddebug/oops/DSA/practice.py
@@ -1,3 +1,4 @@
+# method 1
 num = [1, 2, 1, 2, 4, 5, 7, 8, 7, 8, 9, 9]
 freq_map = {}
 for i in range(0, len(num)):
@@ -6,3 +7,10 @@ for i in range(0, len(num)):
     else:
         freq_map[num[i]] = 1
 print(freq_map)
+# frequency map
+# method 2
+hash_map = {}
+n = len(num)
+for i in range(0, n):
+    hash_map[num[i]] = hash_map.get(num[i], 0) + 1
+print(hash_map)
