32ed73094308f2beb5827e57d5134d1047b21290
Vinayak Singh <vs2622003@gmail.com>
2025-03-22 23:39:06 +0530

    new day

--- codeanddebug/oops/DSA/practice.py
+++ codeanddebug/oops/DSA/practice.py
@@ -1,4 +1,6 @@
+# frequency map
 # method 1
+
 num = [1, 2, 1, 2, 4, 5, 7, 8, 7, 8, 9, 9]
 freq_map = {}
 for i in range(0, len(num)):
@@ -7,10 +9,18 @@ for i in range(0, len(num)):
     else:
         freq_map[num[i]] = 1
 print(freq_map)
-# frequency map
+
+
 # method 2
+
 hash_map = {}
 n = len(num)
 for i in range(0, n):
     hash_map[num[i]] = hash_map.get(num[i], 0) + 1
 print(hash_map)
+
+# method3
+
+from collections import Counter
+
+print(dict(Counter(num)))
